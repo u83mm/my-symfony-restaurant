c2907d9c99f993d5f2937da3beb70dc0df3a863f
Mario Moreno <cursotecnoweb@gmail.com>
2025-05-08 18:57:17 +0200

    feat: implement translation to 'our menu' view

--- src/Controller/MenuCategoriesController.php
+++ src/Controller/MenuCategoriesController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/menu/categories')]
+#[Route('/{_locale}/menu/categories')]
 class MenuCategoriesController extends AbstractController
 {  
     #[Route('/{category}', name: 'app_dishes_category')]
