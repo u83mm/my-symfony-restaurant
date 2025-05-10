266b5458fb1603f69d24a207fcb93d9c30ba9f18
Mario Moreno <cursotecnoweb@gmail.com>
2025-05-10 11:00:42 +0200

    feat: implement translations in reservations view

--- src/Form/ReservationsType.php
+++ src/Form/ReservationsType.php
@@ -8,53 +8,74 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ReservationsType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+{        
+    // The session is used to get the locale for the placeholder
+    private Session $session;
+    private array $timeSelect = [            
+        '12:00'  => '12:00',
+        '12:30'  => '12:30',
+        '13:00'  => '13:00',
+        '13:30'  => '13:30',
+        '14:00'  => '14:00',
+        '14:30'  => '14:30',
+        '15:00'  => '15:00',
+    ];
+
+    private array $peopleSelect = [        
+        '1'      => 1,
+        '2'      => 2,
+        '3'      => 3,
+        '4'      => 4,
+        '5'      => 5,
+        '6'      => 6,
+        '7'      => 7,
+        '8'      => 8,
+        '9'      => 9,
+        '10'     => 10,
+        '11'     => 11,
+        '12'     => 12,
+        '13'     => 13,
+        '14'     => 14,
+        '15'     => 15,
+        '16'     => 16,
+        '17'     => 17,
+        '18'     => 18,
+        '19'     => 19,
+        '20'     => 20
+    ];
+
+    private string $timeSelectPlaceholder = '';
+    private string $peopleSelectPlaceholder = '';
+
+    public function __construct()
     {
+        // Set up the session
+        $this->session = new Session();
+        
+        // Set up the time and people select placeholders
+        $this->timeSelectPlaceholder = $this->session->get('_locale') == 'en' ? 'Select' : 'Seleccionar';
+        $this->peopleSelectPlaceholder = $this->session->get('_locale') == 'en' ? 'Select' : 'Seleccionar';
+    }    
+
+   
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {        
         $builder
             ->add('date', DateType::class, [
                 'widget' => 'single_text',
             ])
             ->add('time', ChoiceType::class, [
-                'choices' => [
-                    'Select' => "",
-                    '12:00'  => '12:00',
-                    '12:30'  => '12:30',
-                    '13:00'  => '13:00',
-                    '13:30'  => '13:30',
-                    '14:00'  => '14:00',
-                    '14:30'  => '14:30',
-                    '15:00'  => '15:00',
-                ]
+                'choices' => $this->timeSelect,
+                'placeholder' => $this->timeSelectPlaceholder,
             ])
             ->add('name')
             ->add('peopleQty', ChoiceType::class, [
-                'choices' => [
-                    'Select' => "",
-                    '1'      => 1,
-                    '2'      => 2,
-                    '3'      => 3,
-                    '4'      => 4,
-                    '5'      => 5,
-                    '6'      => 6,
-                    '7'      => 7,
-                    '8'      => 8,
-                    '9'      => 9,
-                    '10'     => 10,
-                    '11'     => 11,
-                    '12'     => 12,
-                    '13'     => 13,
-                    '14'     => 14,
-                    '15'     => 15,
-                    '16'     => 16,
-                    '17'     => 17,
-                    '18'     => 18,
-                    '19'     => 19,
-                    '20'     => 20
-                ]
+                'choices' => $this->peopleSelect,
+                'placeholder' => $this->peopleSelectPlaceholder,
             ])
             ->add('email')
             ->add('comment')
