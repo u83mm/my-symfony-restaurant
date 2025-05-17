fbbc1a8e60c303936bd7f2aa7271d173d9ef35dd
Mario Moreno <cursotecnoweb@gmail.com>
2025-05-17 13:48:15 +0200

    feat: improve translation language in edit dish view

--- src/Form/DishType.php
+++ src/Form/DishType.php
@@ -5,12 +5,12 @@ namespace App\Form;
 use App\Entity\Dish;
 use App\Entity\DishDay;
 use App\Entity\DishMenu;
-use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
@@ -19,32 +19,22 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 class DishType extends AbstractType
 {        
     public function __construct(
-        private TranslatorInterface $translator,
-        private EntityManagerInterface $em,
-        private array $dishDayChoices = [],
-        private array $dishMenuChoices = []                  
-        
+        private TranslatorInterface $translator,                                          
     )   
-    {   
-        // Set up the option fields for the select elements       
-        foreach ($this->em->getRepository(DishDay::class)->findAll() as $key => $value) {    
-            $this->dishDayChoices[ucfirst($translator->trans($value->getCategoryName()))] = $key;
-        }       
-
-        foreach ($this->em->getRepository(DishMenu::class)->findAll() as $key => $value) {
-            $this->dishMenuChoices[ucfirst($translator->trans($value->getMenuCategory()))] = $key;
-        }
+    {             
     }
+   
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+    {       
         $builder
             ->add('name')
-            ->add('description', null, [
+            ->add('description', TextareaType::class, [
                 'attr' => [
                     'cols'  => "30",
                     'rows'  => "10",
-                ]
+                ],
+                'data' => $this->translator->trans($options['data']->getDescription() ?? '')               
             ])            
             ->add('price', NumberType::class, [
                 'html5' => true,
@@ -67,15 +57,21 @@ class DishType extends AbstractType
                     'value' => 0,
                 ],
                 'required' => false,
+            ])            
+            ->add('dishDay', EntityType::class, [
+                'class' => DishDay::class,
+                'choice_label' => function(Dishday $dishDay) {
+                    return ucfirst($this->translator->trans($dishDay->getCategoryName()));
+                }
             ])
-            ->add('dishDay', ChoiceType::class, [                
-                'choices' => $this->dishDayChoices
-            ])
-            ->add('dishMenu', ChoiceType::class, [
-                'label' => 'Category',
-                'choices' => $this->dishMenuChoices
-            ])
+            ->add('dishMenu', EntityType::class, [
+                'class' => DishMenu::class,
+                'choice_label' => function(DishMenu $dishMenu) {
+                    return ucfirst($this->translator->trans($dishMenu->getMenuCategory()));
+                }
+            ])          
             ->add('picture', null, [
+                'mapped' => false,
                 'label' => false,
                 'attr' => [
                     'hidden' => true
